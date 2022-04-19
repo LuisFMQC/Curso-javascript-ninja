d865ddc117124feeb21fb69cd999ade52fb540fc
Luis Fernando <luismqdf@gmail.com>
2022-04-19 01:38:26 -0300

    Calculadora

--- Javascript-ninja/challenges/main.js
+++ Javascript-ninja/challenges/main.js
@@ -1,12 +1,11 @@
 (function(win, doc){
     'use strict';
 
-    function sum(){
-      return Array.prototype.reduce.call( arguments, function( acumulated, actual, index){
-        return +acumulated + +actual
-      });
+    console.time('Calculando tempo do for');
+    for(var i = 0; i < 10000; i++){
+      console.log( 'calculando...' );
     }
 
-    console.log( sum(1, 2, 3, 4, 5) );
+    console.timeEnd('Calculando tempo do for');
 
 })(window, document);
