1671f91f6b548a284e39ae469d6cf7d662bacae5
Luis Fernando <luismqdf@gmail.com>
2022-04-26 18:00:53 -0300

    ajax

--- Javascript-ninja/challenges/main.js
+++ Javascript-ninja/challenges/main.js
@@ -1,11 +1,19 @@
 (function(win, doc){
     'use strict';
 
-    console.time('Calculando tempo do for');
-    for(var i = 0; i < 10000; i++){
-      console.log( 'calculando...' );
+ var ajax = new XMLHttpRequest();
+ ajax.open('GET', 'data/data.json')
+ ajax.send();
+
+ console.log('Carregando...')
+
+  ajax.addEventListener('readystatechange', function() {
+    if( isRequestOk() ){
+      console.log('Requisição ok :)');
     }
+  }, false)
 
-    console.timeEnd('Calculando tempo do for');
-
+  function isRequestOk(){
+    return ajax.readyState === 4 && ajax.status === 200;
+  }
 })(window, document);
